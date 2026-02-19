03d1696f7604aef20f4c06e8899c10bc378e87d0
Jaedon <Jsatchell2017@gmail.com>
2026-02-19 11:25:58 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// Initialize DOM elements
 const clickDisplay = document.getElementById("click-display");
 const clickButton = document.getElementById("click-button");
+
+// Track count with variable
 let count = 0;
+
+// Increment count with click event listener
 clickButton.addEventListener("click", () => {
   count += 1;
   clickDisplay.innerHTML = `Click Count: ${count}`;
