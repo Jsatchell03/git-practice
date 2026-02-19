78169c54fc543c628f08892b2b1221248d516443
Jsatchell03 <jsatchell2017@gmail.com>
2026-02-19 13:13:23 -0500

    Added reset functionality and styled buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Initialize DOM elements
 const clickDisplay = document.getElementById("click-display");
 const clickButton = document.getElementById("click-button");
-
+const resetButton = document.getElementById("reset-button");
 // Track count with variable
 let count = 0;
 
@@ -9,4 +9,11 @@ let count = 0;
 clickButton.addEventListener("click", () => {
   count += 1;
   clickDisplay.innerHTML = `Click Count: ${count}`;
+});
+
+// Reset count with click event listener on reset button
+resetButton.addEventListener("click", () => {
+  count = 0;
+  clickDisplay.innerHTML = `Click Count: ${count}`;
+  alert("Count reset to 0");
 });
